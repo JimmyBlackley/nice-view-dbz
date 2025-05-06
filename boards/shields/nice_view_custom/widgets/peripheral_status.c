b333a0ba6e68a02e8e9ec095072126982bea3cac
James <jblackley97@gmail.com>
2025-05-06 11:33:44 +1000

    test art

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -23,8 +23,31 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(mountain);
+LV_IMG_DECLARE(f01);
+LV_IMG_DECLARE(f02);
+LV_IMG_DECLARE(f03);
+LV_IMG_DECLARE(f04);
+LV_IMG_DECLARE(f05);
+LV_IMG_DECLARE(f06);
+LV_IMG_DECLARE(f07);
+LV_IMG_DECLARE(f08);
+LV_IMG_DECLARE(f09);
+LV_IMG_DECLARE(f10);
+LV_IMG_DECLARE(f11);
+
+const lv_img_dsc_t *anim_imgs[] = {
+    &f01,
+    &f02,
+    &f03,
+    &f04,
+    &f05,
+    &f06,
+    &f07,
+    &f08,
+    &f09,
+    &f10,
+    &f11,
+};
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -114,11 +137,19 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    //lv_obj_t *art = lv_img_create(widget->obj);
+    //bool random = sys_rand32_get() & 1;
+    //lv_img_set_src(art, random ? &balloon : &mountain);
+    //lv_img_set_src(art, &f01);
 
+    lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
+    lv_obj_center(art);                                         //<--
+    lv_animimg_set_src(art, (const void **) anim_imgs, 11);     //<--
+    lv_animimg_set_duration(art, 3000);                         //<--
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
+    lv_animimg_start(art);                                      //<--
+
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
     widget_peripheral_status_init();
